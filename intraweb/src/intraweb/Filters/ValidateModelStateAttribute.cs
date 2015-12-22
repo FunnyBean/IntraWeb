9b255237fafdea9557775e53af78f6cd5badd78f
Miňo <mino.martiniak@gmail.com>
2015-12-22 14:43:29 +0100

    Create Action filters for valdationg and checking null argument

---
+++ intraweb/src/intraweb/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Filters;
+
+namespace intraweb.Filters
+{
+    /// <summary>
+    /// Action filter attribute for model validating.
+    /// </summary>
+    /// <remarks>
+    /// If model state is not valid, then action result is set to badRequest.
+    /// </remarks>
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Called when [action executing].
+        /// </summary>
+        /// <param name="actionContext">The action context.</param>
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {            
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Result = new BadRequestObjectResult(actionContext.ModelState);
+            }
+        }
+    }
+}
