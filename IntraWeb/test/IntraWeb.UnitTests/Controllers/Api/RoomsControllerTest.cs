76d3558cbeb64f14e5ee059ed0b374d28ad73eb2
Miňo <mino.martiniak@gmail.com>
2016-01-17 10:48:49 +0100

    Create unittesting project

---
+++ IntraWeb/test/IntraWeb.UnitTests/Controllers/Api/RoomsControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using IntraWeb.Controllers;
+
+namespace IntraWeb.UnitTests.Controllers.Api
+{
+    public class RoomsControllerTest
+    {
+        #region "Get rooms"
+
+        [Fact]
+        public void GetRoomsReturnEmptyListWhenRoomsDoesntExist()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        #endregion
+
+    }
+}
