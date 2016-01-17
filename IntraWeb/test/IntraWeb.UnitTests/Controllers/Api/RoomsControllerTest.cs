ab3f6ebb613402ce1fc147a60c07cd26b29646ad
Miňo <mino.martiniak@gmail.com>
2016-01-17 15:08:09 +0100

    Create first anit tests for RoomsController

--- IntraWeb/test/IntraWeb.UnitTests/Controllers/Api/RoomsControllerTest.cs
+++ IntraWeb/test/IntraWeb.UnitTests/Controllers/Api/RoomsControllerTest.cs
@@ -4,17 +4,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using IntraWeb.Controllers;
+using IntraWeb.Models.Dummies;
+using IntraWeb.UnitTests.Service;
+using IntraWeb.ViewModels.Administration;
 
 namespace IntraWeb.UnitTests.Controllers.Api
 {
     public class RoomsControllerTest
     {
+
+        public RoomsControllerTest()
+        {
+            AdministrationModelMapping.ConfigureRoomMapping();
+        }
+
         #region "Get rooms"
 
         [Fact]
-        public void GetRoomsReturnEmptyListWhenRoomsDoesntExist()
+        public void GetRoomsReturnEmptyListWhenRoomsDoesnotExist()
         {
-            Assert.Equal(1, 1);
+            // Arrange
+            var target = CreateRoomsController(null);
+
+            // Act
+            var roomsCount = target.Get().Count();
+
+            // Assert            
+            Assert.Equal(0, roomsCount);
+        }
+
+        [Fact]
+        public void GetRoomsReturnAllRoomsFromRepository()
+        {
+            // Arrange
+            var target = CreateRoomsController((rep) =>
+            {
+                rep.AddRoom(new Models.Room()
+                {
+                    Name = "First",
+                    Description = "First room"
+                });
+                rep.AddRoom(new Models.Room()
+                {
+                    Name = "Second",
+                    Description = "Second room"
+                });
+            });
+
+            // Act 
+            var rooms = target.Get().ToList();
+
+            // Assert
+            Assert.Equal(2, rooms.Count);
+
+            Assert.Equal(0, rooms[0].Id);
+            Assert.Equal("First", rooms[0].Name);
+            Assert.Equal("First room", rooms[0].Description);
+        }
+
+        #endregion
+
+
+        #region "Helpers"
+
+        private RoomsController CreateRoomsController(Action<RoomDummyRepository> initRepository)
+        {
+            var logger = new StubLogger<RoomsController>();
+            var roomsRepository = new RoomDummyRepository();
+            roomsRepository.ClearAll();
+
+            if (initRepository != null)
+            {
+                initRepository(roomsRepository);
+            }
+
+            var target = new RoomsController(roomsRepository, logger);
+
+            return target;
         }
 
         #endregion
