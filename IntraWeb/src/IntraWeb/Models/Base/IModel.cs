4d3d12f4a320d284b8596dad4da151d609eaf8a4
Miňo <mino.martiniak@gmail.com>
2016-02-23 21:13:27 +0100

    Refactor - remove unnecessary usings

--- IntraWeb/src/IntraWeb/Models/Base/IModel.cs
+++ IntraWeb/src/IntraWeb/Models/Base/IModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace IntraWeb.Models.Base
+﻿namespace IntraWeb.Models.Base
 {
     /// <summary>
     /// Interface for Db models.
