635eced6a8788bf269122a8484a7b73dfab65cd2
Stano Peťko <stano@stanopetko.eu>
2016-03-04 18:39:38 +0100

    Opravené vytváranie textovej časti emailu, pridaný test.

--- IntraWeb/src/IntraWeb/Services/Email/HtmlEmailCreator.cs
+++ IntraWeb/src/IntraWeb/Services/Email/HtmlEmailCreator.cs
@@ -12,12 +12,17 @@ namespace IntraWeb.Services.Emails
         public MimeMessage CreateEmail(string htmlBody)
         {
             var msg = new MimeMessage();
-
             var titleMatch = _reSubject.Match(htmlBody);
             if (titleMatch.Success)
             {
                 msg.Subject = titleMatch.Groups[1].Value;
             }
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = htmlBody;
+            builder.TextBody = CreateTextBody(htmlBody);
+
+            msg.Body = builder.ToMessageBody();
 
             return msg;
         }
@@ -31,23 +36,25 @@ namespace IntraWeb.Services.Emails
         private Regex _reNewLines = new Regex(@"[\r\n]+");
         private Regex _reParagraphs = new Regex(@"<(/?p|/?h\d|li|br|/tr)[ >/]", RegexOptions.IgnoreCase);
         private Regex _reHtmlTags = new Regex(@"<[^>]*>");
-        private Regex _reStartWhitespace = new Regex(@"^[ \t]+", RegexOptions.Multiline);
+        private Regex _reLeadingWhitespace = new Regex(@"^[ \t]+", RegexOptions.Multiline);
+        private Regex _reTrailingWhitespace = new Regex(@"[ \t]+\r?\n", RegexOptions.Multiline);
         private Regex _reWhitespace = new Regex(@"[ \t]+");
 
-        public string CreateTextBody(string htmlBody)
+        private string CreateTextBody(string htmlBody)
         {
             var textBody = htmlBody;
             textBody = _reHeader.Replace(textBody, string.Empty);
             textBody = _reTables.Replace(textBody, " $0");
             textBody = _reLinks.Replace(textBody, "${text} (${href})");
             textBody = _reNewLines.Replace(textBody, " ");
-            textBody = _reParagraphs.Replace(textBody, "\n$0");
+            textBody = _reParagraphs.Replace(textBody, "\r\n$0");
             textBody = _reHtmlTags.Replace(textBody, string.Empty);
             textBody = System.Net.WebUtility.HtmlDecode(textBody);
-            textBody = _reStartWhitespace.Replace(textBody, string.Empty);
+            textBody = _reLeadingWhitespace.Replace(textBody, string.Empty);
+            textBody = _reTrailingWhitespace.Replace(textBody, "\r\n");
             textBody = _reWhitespace.Replace(textBody, " ");
 
-            return textBody;
+            return textBody.Trim();
         }
 
     }
---
+++ IntraWeb/test/IntraWeb.UnitTests/Service/Email/HtmlEmailCreatorTests.cs
@@ -0,0 +1,48 @@
+﻿using IntraWeb.Services.Emails;
+using MimeKit;
+using Xunit;
+
+namespace IntraWeb.UnitTests.Service.Email
+{
+    public class HtmlEmailCreatorTests
+    {
+
+        private readonly string _htmlBody =
+@"<DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
+    <title>Lorem ipsum</title>
+</head>
+<body>
+   <h1>Main Header</h1>
+   <p>Paragraph 1.
+Second line with link: <a href=""http://example.com"">link to example</a>.
+Last line.  </p>
+
+<p>Paragraph 2.</p>
+</body>";
+
+        private readonly string _textBody =
+@"Main Header
+
+Paragraph 1. Second line with link: link to example (http://example.com). Last line.
+
+Paragraph 2.";
+
+
+        [Fact]
+        public void ShouldCreateMessage()
+        {
+            var creator = new HtmlEmailCreator();
+            var msg = creator.CreateEmail(_htmlBody);
+
+            Assert.Equal("Lorem ipsum", msg.Subject);
+
+            var body = msg.Body as MultipartAlternative;
+            Assert.Equal(_htmlBody, body.HtmlBody);
+            Assert.Equal(_textBody, body.TextBody);
+        }
+
+    }
+}
