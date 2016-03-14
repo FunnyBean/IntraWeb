0d233a1cc6155e74a374a319c73998ff046a81d1
Stano Peťko <stano@stanopetko.eu>
2016-03-14 18:23:35 +0100

    Rozhranie a základná trieda pre dáta emailov.

---
+++ IntraWeb/src/IntraWeb/Services/Email/BaseEmailData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace IntraWeb.Services.Email
+{
+    public class BaseEmailData
+        : IEmailData
+    {
+
+        #region Constructors
+
+        public BaseEmailData() { }
+
+        public BaseEmailData(string emailType)
+        {
+            this.EmailType = EmailType;
+        }
+
+        #endregion
+
+
+        #region IEmailData
+
+        private List<string> _to = new List<string>();
+        private List<string> _cc = new List<string>();
+        private List<string> _bcc = new List<string>();
+
+        public string EmailType { get; set; }
+        public string From { get; set; }
+        public IEnumerable<string> To { get { return _to; } }
+        public IEnumerable<string> Cc { get { return _cc; } }
+        public IEnumerable<string> Bcc { get { return _bcc; } }
+        public string ReplyTo { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ IntraWeb/src/IntraWeb/Services/Email/IEmailData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace IntraWeb.Services.Email
+{
+    public interface IEmailData
+    {
+
+        string EmailType { get; set; }
+
+        string From { get; }
+        IEnumerable<string> To { get; }
+        IEnumerable<string> Cc { get; }
+        IEnumerable<string> Bcc { get; }
+        string ReplyTo { get; }
+
+    }
+}
