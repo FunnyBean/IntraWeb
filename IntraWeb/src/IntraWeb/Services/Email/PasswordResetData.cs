ee2c37af49126c7ac04237e62d5ae4c99cd24970
Stano Peťko <stano@stanopetko.eu>
2016-03-14 18:27:23 +0100

    Trieda pre dáta emailu na zmenu hesla.

---
+++ IntraWeb/src/IntraWeb/Services/Email/PasswordResetData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntraWeb.Services.Email
+{
+    public class PasswordResetData
+        : BaseEmailData
+    {
+
+        #region Constructors
+
+        public PasswordResetData(string emailType, string passwordResetLink)
+        {
+            this.EmailType = emailType;
+            this.PasswordResetLink = passwordResetLink;
+        }
+
+        #endregion
+
+
+        #region General
+
+        [EmailDataKey("PasswordResetLink")]
+        public string PasswordResetLink { get; set; }
+
+        #endregion
+
+    }
+}
